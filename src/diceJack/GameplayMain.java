db1b119459592ce3adae2ec516d0e4c933282f0e
huy <htngo23@gmail.com>
2016-03-08 17:05:06 -0600

    pulling

--- src/diceJack/GameplayMain.java
+++ src/diceJack/GameplayMain.java
@@ -1,5 +1,15 @@
 package diceJack;
+import java.util.Scanner;
+
 
 public class GameplayMain {
+	Scanner leer = new Scanner(System.in);
+	int aimscore;
 
-}
+	public void Selectlimit() {
+		System.out.println("What do you want to play until?");
+		aimscore = leer.nextInt();
+		System.out.println("We will play until" + aimscore + "points");
+	}
+	public void 
+}}
