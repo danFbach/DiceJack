0f58fe913b13f0df269a2de90aa0ae821eaed583
huy <htngo23@gmail.com>
2016-03-08 17:18:34 -0600

    more pulling

--- src/diceJack/GameplayMain.java
+++ src/diceJack/GameplayMain.java
@@ -6,10 +6,6 @@ public class GameplayMain {
 	Scanner leer = new Scanner(System.in);
 	int aimscore;
 
-	public void Selectlimit() {
-		System.out.println("What do you want to play until?");
-		aimscore = leer.nextInt();
-		System.out.println("We will play until" + aimscore + "points");
-	}
+
 	public void 
 }}
