b5c5c49d17bd114d915bbf1f22e1ebf370ad34ff
danFbach <quiktekrepair@gmail.com>
2016-03-09 10:46:15 -0600

    merge fix

--- src/diceJack/DiceJackMain.java
+++ src/diceJack/DiceJackMain.java
@@ -1,22 +1,7 @@
 package diceJack;
 
 
-public class DiceJackMain {Integer result = 0;
-		
 
-	public static void main(String[] args) {
-				
-
-		
-
-		GameplayMain p1 = new GameplayMain();
-		
-		
-		
-
-			
-			
-			//p1.limit();// must go outside of loop
 
 public class DiceJackMain {
 	
