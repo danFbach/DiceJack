d73010687bf4267fd64f6311bb646ed7ac9beb4c
huy <htngo23@gmail.com>
2016-03-09 00:31:50 -0600

    compressed into methods.  working on setting score limit

--- src/diceJack/GameplayMain.java
+++ src/diceJack/GameplayMain.java
@@ -6,6 +6,7 @@ public class GameplayMain extends DiceRoll {
 	Scanner leer = new Scanner(System.in);
 
 	Integer result = 0;
+	Integer result2 = 0;
 	Integer choice1;
 	Integer total = 0;
 	Integer total2 = 0;
@@ -15,29 +16,37 @@ public class GameplayMain extends DiceRoll {
 	// CurrentScores score = new CurrentScores();
 	DiceRoll choice = new DiceRoll();
 	DiceRoll choice2 = new DiceRoll();
+	Integer scorereach;
 	// boolean taco;
 	// taco = true;
 
 	DiceJackMain sum1 = new DiceJackMain();
 
 	public int gameplay1() {
-		points = sum1.pointone();
+		points = choice.funn(result);
 		total = total + points;
 		System.out.println("p1 You've rolled a: " + points + " Your total: " + total);
 		return total;
 	}
 	public int gameplay2(){
-		points2 = choice2.funn(result);
+		points2 = choice2.funn(result2);
 		total2 = total2 + points2;
-		System.out.println("p2 You've rolled a: " + points2 + " Your total: "+ total);
+		System.out.println("p2 You've rolled a: " + points2 + " Your total: "+ total2);
+		return total2;
+	}
+	public int limit(){
+		System.out.println("what do you want to play until?");
+		int scorereach = leer.nextInt();
+		return scorereach;
+	}
+	public int startif(){
+		while((total < scorereach) || (total2 < scorereach)){
+			gameplay1();
+			gameplay2();
+			}
 		return total;
 	}
-
-
-
-
-public int pointone() {
-	points = choice.funn(result);
-	return total;
-}
+	public void end(){
+		System.out.println("We have a winner");
+	}
 }
---
+++ src/diceJack/Player.java
@@ -0,0 +1,37 @@
+package diceJack;
+
+public class Player {
+	
+	String playerName;
+	
+	Integer currentScore;
+	
+	boolean isPlaying;
+	
+	Integer result = 0;
+	Integer result2 = 0;
+	Integer choice1;
+	Integer whatever;
+	Integer current = 0;
+	CurrentScores score = new CurrentScores();
+	DiceRoll choice = new DiceRoll();
+	
+	public void humanPlayer(String PlayerName, int CurrentScore, boolean IsPlaying){
+		
+		playerName = PlayerName;
+		currentScore = CurrentScore;
+		isPlaying = IsPlaying;
+		
+		
+		
+	}
+	public void aiPlayer(String PlayerName, int CurrentScore, boolean IsPlaying){
+		
+		playerName = PlayerName;
+		currentScore = CurrentScore;
+		isPlaying = IsPlaying;
+		
+	}
+	
+
+}
